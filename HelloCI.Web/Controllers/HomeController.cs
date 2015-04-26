27d47976608a8b1aab6c224d2cbc2b8f21316d76
Ken <408377476@qq.com>
2015-04-27 02:56:42 +0800

    ddfdf

--- HelloCI.Web/Controllers/HomeController.cs
+++ HelloCI.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace HelloCI.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message;= "Welcome to ASP.NET MVC!";
 
             return View();
         }
