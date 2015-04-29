4a6d5c6cc796795b5c02c40150ecaf58aa3b0a77
Ken <408377476@qq.com>
2015-04-29 15:31:48 +0800

    add ms unit test

---
+++ HelloCI.Web.UnitTests/ControllerTests/MSUnitTestClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloCI.Web.UnitTests.ControllerTests
+{
+    [TestClass]
+    public class MSUnitTestClass
+    {
+        [TestMethod]
+        public void TestFail()
+        {
+            Assert.Fail("fail forever.");
+        }
+    }
+}
